839f580edda3308c3626185ffe6f610cdbaa772f
Prasun Neupane <prasunneupane14@gmail.com>
2026-03-30 20:41:52 +0545

    feat: add exam details template to Show.vue

--- resources/js/pages/exams/Show.vue
+++ resources/js/pages/exams/Show.vue
@@ -1,1 +1,12 @@
 <!-- Show Exam -->
+<template>
+  <div>
+    <h1>Exam Details</h1>
+    <p><strong>Name:</strong> 1</p>
+    <p><strong>Academic Year:</strong> 2</p>
+    <p><strong>Class:</strong> 3</p>
+    <p><strong>Section:</strong> 4</p>
+    <p><strong>Date:</strong> 5</p>
+    <p><strong>Description:</strong> 6</p>
+  </div>
+</template>
